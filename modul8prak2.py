c2263d63c5be7d45e28d6cb71be3be11e6df1c08
Tapolu <132997752+Tapolu@users.noreply.github.com>
2023-11-16 20:36:27 +0700

    Add files via upload

---
+++ modul8prak2.py
@@ -0,0 +1,13 @@
+def perpangkatan(x, pangkat):
+    if pangkat == 0:
+        return 1.0
+    else:
+        return x * perpangkatan(x, pangkat - 1) if pangkat > 0 else 1.0 / perpangkatan(x, -pangkat)
+
+angka = float(input("Masukkan angka = "))
+pangkat = int(input("Masukkan pangkat (positif atau negatif) = "))
+
+
+hasil_perpangkatan = perpangkatan(angka, pangkat)
+
+print(f"{angka} pangkat {pangkat} = {hasil_perpangkatan}")
